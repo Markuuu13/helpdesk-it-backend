45f601dc04cbac873053a416b6a9e736ee229b02
Markuuu13 <markjuscardeno13@gmail.com>
2026-07-08 20:37:01 +0800

    feat: public api view for no authentication views

--- helpdesk_auth/configs/custom_managers.py
+++ helpdesk_auth/configs/custom_managers.py
@@ -1,4 +1,10 @@
 from django.db import models
+from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
+
+class PublicAPIView(APIView):
+    permission_classes = [AllowAny]
+    authentication_classes = []
 
 class ActiveManager(models.Manager):
     def get_queryset(self):
