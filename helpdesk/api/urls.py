a97663dc7374820d5188ba8cf78c820c41a59eb2
Markuuu13 <markjuscardeno13@gmail.com>
2026-07-21 00:42:30 +0800

    feat: Ticket Detail url added

--- helpdesk/api/urls.py
+++ helpdesk/api/urls.py
@@ -2,9 +2,10 @@
 
 from django.urls import include, path
 
-from helpdesk.api.ticket import TicketListView
+from helpdesk.api.ticket import TicketDetailView, TicketListView
 
 
 urlpatterns = [
     path('tickets/', TicketListView.as_view(), name='ticket-list'),
+    path('tickets/<int:ticket_number>/', TicketDetailView.as_view(), name='ticket-detail'),
 ]
