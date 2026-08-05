d92996bb6bcf933ca4eb8266c986841dbc96fa50
Markuuu13 <markjuscardeno13@gmail.com>
2026-08-06 01:32:24 +0800

    feat: added TicketCreateSerializer

--- helpdesk/serializers/ticket_serializer.py
+++ helpdesk/serializers/ticket_serializer.py
@@ -1,6 +1,9 @@
 
 from helpdesk.models import Ticket
 from rest_framework import serializers
+from django.db import transaction
+from django.utils import timezone
+import uuid
 
 
 class TicketListSerializer(serializers.ModelSerializer):
@@ -9,10 +12,8 @@ class TicketListSerializer(serializers.ModelSerializer):
         fields = [
             'ticket_number',
             'title',
-            'description',
             'created_by',
             'created_at',
-            'updated_at',
             'asset',
             'status'
         ]
@@ -21,3 +22,27 @@ class TicketDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ticket
         fields = '__all__'
+
+class TicketCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ticket
+        fields = [
+            'title',
+            'description',
+            'asset',
+        ]
+        
+    def create(self, validated_data):
+        # Generate a unique ticket number
+        validated_data['ticket_number'] = self.generate_ticket_number()
+        return super().create(validated_data)
+
+    @staticmethod
+    def generate_ticket_number():
+        year = timezone.now().year
+        prefix = f"TCKT{year}"
+        
+        while True:
+            ticket = f"{prefix}{uuid.uuid4().hex[:8].upper()}"
+            if not Ticket.objects.filter(ticket_number=ticket).exists():
+                return ticket
