9c32c311c80fc494a28c297ec35e870b535ce69f
Markuuu13 <markjuscardeno13@gmail.com>
2026-08-06 01:31:23 +0800

    moved urls.py file

--- helpdesk/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import include, path
-
-from helpdesk.api.ticket import TicketDetailView, TicketListView
-
-
-urlpatterns = [
-    path('tickets/', TicketListView.as_view(), name='ticket-list'),
-    path('tickets/<int:ticket_number>/', TicketDetailView.as_view(), name='ticket-detail'),
-]
--- helpdesk_auth/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import include, path
-
-from helpdesk_auth.api.user_view import LoginView, SignupView
-
-
-urlpatterns = [
-    path('login/', LoginView.as_view(), name='login'),
-    path('signup/', SignupView.as_view(), name='signup')
-]
